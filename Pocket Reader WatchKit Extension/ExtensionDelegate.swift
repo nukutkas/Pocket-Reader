8c11f73e9ea0f497c56deb24f043c1c7f11013bc
Nikita Kochetov <nothsmple@mail.ru>
2021-07-11 23:07:14 +0300

    Fix.

--- Pocket Reader WatchKit Extension/ExtensionDelegate.swift
+++ Pocket Reader WatchKit Extension/ExtensionDelegate.swift
@@ -10,6 +10,5 @@ import WatchKit
 class ExtensionDelegate: NSObject, WKExtensionDelegate {
 
     func applicationDidFinishLaunching() {
-        // Perform any final initialization of your application.
     }
 }
