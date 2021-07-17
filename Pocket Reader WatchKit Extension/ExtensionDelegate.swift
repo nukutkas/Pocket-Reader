751d15a193309431192d8e4c36a076ce2aed1da1
Nikita Kochetov <nothsmple@mail.ru>
2021-07-17 20:24:03 +0300

    Function setupWatchConnectivity has been made in Extension delegate.

--- Pocket Reader WatchKit Extension/ExtensionDelegate.swift
+++ Pocket Reader WatchKit Extension/ExtensionDelegate.swift
@@ -6,9 +6,33 @@
 //
 
 import WatchKit
+import WatchConnectivity
 
 class ExtensionDelegate: NSObject, WKExtensionDelegate {
 
     func applicationDidFinishLaunching() {
+        setupWatchConnectivity()
     }
+    
+    func setupWatchConnectivity(){
+        if WCSession.isSupported() {
+            let session = WCSession.default
+            session.delegate = self
+            session.activate()
+        }
+    }
+}
+
+
+
+extension ExtensionDelegate: WCSessionDelegate {
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        if let error = error {
+            print("WC Session activation failed with error: \(error.localizedDescription)")
+            return
+        }
+        print("WC Session activated with state: \(activationState.rawValue)")
+    }
+    
+
 }
